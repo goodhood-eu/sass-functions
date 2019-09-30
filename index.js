2b1e9cb04e1a609055bde0a7504d06b845e1269b
Max Degterev <max@degterev.me>
2019-09-30 17:13:51 +0200

    better index file structure

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const modules = [
+const paths = [
   'replace',
   'image-size',
   'image-width',
   'image-height',
 ];
 
-module.exports = modules.reduce((acc, name) => ({ ...acc, [name]: require(`./${name}`) }), {});
+const modules = paths.reduce((acc, name) => ({ ...acc, [name]: require(`./${name}`) }), {});
+const getFn = (options) => Object.keys(modules).map((name) => modules[name](options));
+
+getFn.modules = modules;
+
+module.exports = getFn;
