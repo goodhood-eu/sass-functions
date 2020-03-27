a86e7ba4cb85d7c4f6cdd734b6c76731a4e0b5c9
Max Degterev <max@degterev.me>
2020-03-27 14:43:42 +0100

    fixed

--- utils/index.js
+++ utils/index.js
@@ -1,15 +1,16 @@
+const { resolve } = require('path');
 const escapeStringRegexp = require('escape-string-regexp');
 
 const REGEX_ABSOLUTE_PATH = /^\//;
 
 const getFilePath = ({ alias, publicRoot }, url) => {
-  if (REGEX_ABSOLUTE_PATH.test(url)) return `${publicRoot || __dirname}/${url}`;
+  if (REGEX_ABSOLUTE_PATH.test(url)) return resolve(`${publicRoot || __dirname}/${url}`);
 
   if (alias) {
-    return Object.keys(alias).reduce((acc, key) => {
-      const regex = new RegExp(`^${escapeStringRegexp(key)}`);
-      return acc.replace(regex, alias[key]);
-    }, url);
+    for (const key of Object.keys(alias)) {
+      const regex = new RegExp(`^~${escapeStringRegexp(key)}(\\/|$)`);
+      if (regex.test(url)) return resolve(url.replace(regex, `${alias[key]}$1`));
+    }
   }
 
   return url;
